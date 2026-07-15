f63b6ad2ba14cc4b196934f4455a980f79042eb9
Nihar151 <gangadharannihar@gmail.com>
2026-07-15 15:29:41 +0530

    Connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-const express = require("express")
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+mongoose
+  .connect(procces.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
